06cc6d41abffd19f3389d67bc8622cbd92509e74
Duke jojo <ysha7020@uni.sydney.edu.au>
2022-10-25 11:49:11 +1100

    firebase,react integration

---
+++ art-generator/src/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFireStore} from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-nyG78acuBDV9as9h5Ju9WZG9lEiWJs0",
+  authDomain: "art-generator-ee319.firebaseapp.com",
+  projectId: "art-generator-ee319",
+  storageBucket: "art-generator-ee319.appspot.com",
+  messagingSenderId: "481336666318",
+  appId: "1:481336666318:web:b73beef004eb54359d889f",
+  measurementId: "G-VZB41FQ2E1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFireStore(app);
+const auth = getAuth(app);
+
+export {db, auth};
