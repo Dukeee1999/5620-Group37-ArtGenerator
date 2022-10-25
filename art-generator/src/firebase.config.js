8a9898edd8c2461bf78f9ac629524dec7a98141e
Duke jojo <ysha7020@uni.sydney.edu.au>
2022-10-25 11:53:56 +1100

    add storage

--- art-generator/src/firebase.config.js
+++ art-generator/src/firebase.config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFireStore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
+import {getStorage} from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,5 +25,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFireStore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export {db, auth};
+export {db, auth, storage};
